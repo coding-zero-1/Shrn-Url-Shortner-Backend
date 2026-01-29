a1d3e203ece5200b372591f9f6630d1bf9bfd90d
coding-zero-I <yashsangwan4700@gmail.com>
2026-01-29 11:54:34 +0530

    added dummy function to send email

---
+++ src/utils/sendEmail.ts
@@ -0,0 +1,10 @@
+export function sendVerficationEmail(
+  to: string,
+  subject: string,
+  text: string,
+) {
+  // Placeholder function for sending email
+  console.log(`Sending email to: ${to}`);
+  console.log(`Subject: ${subject}`);
+  console.log(`Text: ${text}`);
+}
