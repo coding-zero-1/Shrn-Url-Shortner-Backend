c056556c9726971423d100f38523181e0f822eca
coding-zero-I <yashsangwan4700@gmail.com>
2026-01-29 11:55:13 +0530

    updated file to handle user routes

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
+import userRouter from "./routes/userRoutes";
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    credentials: true,
+  }),
+);
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+app.use(express.json());
+
+app.use("/api/v1/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
