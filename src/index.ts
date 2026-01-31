f841155b3d5997549d555e1259b69956f277f02d
coding-zero-I <yashsangwan4700@gmail.com>
2026-01-31 11:20:51 +0530

    added redirection endpoint

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import userRouter from "./routes/userRoutes";
 import shortLinkRouter from "./routes/shortLinkRoutes";
 import authMiddleware from "./middleware/authMiddleware";
+import { redirectionController } from "./controllers/redirectionController";
 
 const app = express();
 const PORT = process.env.PORT;
@@ -17,9 +18,10 @@ app.use(
 
 app.use(express.json());
 
-app.use("/api/v1/user", userRouter);
-app.use("/api/v1/shortLink", authMiddleware, shortLinkRouter);
+app.use("/v1/user", userRouter);
+app.use("/re/:shortCode",redirectionController)
+app.use("/v1/shortLink", authMiddleware, shortLinkRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on Port: ${PORT}`);
 });
